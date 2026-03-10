86dd2f5c91a72942714cffb84941e3b1d6c7ebaf
Siddhant <siddhantsrivastava0811@gmail.com>
2026-03-10 22:26:06 +0530

    story of changes

---
+++ src/api/story-of-change/routes/story-of-change.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * story-of-change router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::story-of-change.story-of-change');
